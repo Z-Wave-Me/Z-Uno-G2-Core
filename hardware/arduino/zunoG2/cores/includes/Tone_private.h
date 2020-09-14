2ac4a6801eb9f907269e56603c41fec84983307d
amatilda <kpk@z-wave.me>
2020-09-14 16:52:43 +0300

    fix: analogWrite

---
+++ hardware/arduino/zunoG2/cores/includes/Tone_private.h
@@ -0,0 +1,12 @@
+#ifndef TONE_PRIVATE_H
+#define TONE_PRIVATE_H
+
+#define PWM_TIMER						TIMER1
+#define PWM_TIMER_CLOCK					cmuClock_TIMER1
+#define PWM_DISABLED					0x0
+#define PWM_ENABLED						0xFF
+#define PWM_PIN_DISABLED				0x0
+#define PWM_PIN_ENABLED					0x1
+#define PWM_PIN_PULSE					0x2
+
+#endif //TONE_PRIVATE_H

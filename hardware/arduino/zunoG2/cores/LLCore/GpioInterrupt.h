38e9e0a9295cb329ee7c9218f44aad539c56df8b
amatilda <kpk@z-wave.me>
2020-09-17 21:25:18 +0300

    add pre: GeneralPurposeTimer

--- hardware/arduino/zunoG2/cores/LLCore/GpioInterrupt.h
+++ hardware/arduino/zunoG2/cores/LLCore/GpioInterrupt.h
@@ -1,9 +1,6 @@
 #ifndef GPIO_INTERRPUT_H
 #define GPIO_INTERRPUT_H
 
-typedef void zuno_user_zuno_handler_extint(void);
-
-
 void attachInterrupt(uint8_t interruptPin, void (*userFunc)(void), uint8_t mode);
 void detachInterrupt(uint8_t interruptPin);
 void zunoExtIntMode(uint8_t interruptPin, uint8_t mode);

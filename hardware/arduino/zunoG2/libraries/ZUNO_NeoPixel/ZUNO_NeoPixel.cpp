1049b694ce99ff316a7fb82cf76e2b326222cb26
amatilda <kpk@z-wave.me>
2020-10-03 12:09:20 +0300

    add: ZUNO_LEDS

---
+++ hardware/arduino/zunoG2/libraries/ZUNO_NeoPixel/ZUNO_NeoPixel.cpp
@@ -0,0 +1,17 @@
+#include "Arduino.h"
+#include "stdlib.h"
+#include "ZUNO_NeoPixel.h"
+
+/* Public Constructors */
+NeoPixel::NeoPixel(void) {
+
+}
+
+/* Public Methods */
+
+
+/* Private Methods */
+
+
+/* Preinstantiate Objects */
+NeoPixel Neo = NeoPixel();

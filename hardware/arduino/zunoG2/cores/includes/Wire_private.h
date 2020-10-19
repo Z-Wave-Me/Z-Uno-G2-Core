453cf279c4b33fc9c5617f8c943bc2695339bba4
amatilda <kpk@z-wave.me>
2020-10-19 17:58:19 +0300

    Wire add: transfer and support buffer

--- hardware/arduino/zunoG2/cores/includes/Wire_private.h
+++ hardware/arduino/zunoG2/cores/includes/Wire_private.h
@@ -36,32 +36,36 @@ class TwoWire {
 	public:
 		TwoWire(I2C_TypeDef *i2c);
 		TwoWire(void);
-		void		begin(void);
-		void		begin(uint8_t scl, uint8_t sda);
-		void		begin(uint8_t adress);
-		void		begin(uint8_t adress, uint8_t scl, uint8_t sda);
-		void		beginTransmission(uint8_t adress);
-		void		beginTransmission(uint8_t adress, uint8_t forced_start);
-		size_t		write(uint8_t data);
-		size_t		write(const char *data);
-		size_t		write(const uint8_t *data, size_t quantity);
-		uint8_t		endTransmission(void);
-		uint8_t		endTransmission(uint8_t sendStop);
-		uint8_t		requestFrom(uint8_t address, uint8_t quantity);
-		uint8_t		requestFrom(uint8_t address, uint8_t quantity, uint8_t sendStop);
-		uint8_t		available(void);
-		uint8_t		read(void);
-		void		enableTS(uint8_t on_off);//Не сделанно еще
-		void		setClock(uint32_t clock);
-		void		end(void);
+		ZunoError_t		begin(void);
+		ZunoError_t		begin(uint8_t scl, uint8_t sda);
+		ZunoError_t		begin(uint8_t adress);
+		ZunoError_t		begin(uint8_t adress, uint8_t scl, uint8_t sda);
+		ZunoError_t		begin(uint8_t adress, uint8_t scl, uint8_t sda, void *b, uint16_t len);
+		void			beginTransmission(uint8_t adress);
+		void			beginTransmission(uint8_t adress, uint8_t forced_start);
+		uint8_t			transfer(uint8_t adress, void *b, uint16_t count);
+		uint8_t			transfer(uint8_t adress, void *b);
+		uint8_t			transfer(uint8_t adress, const char *b);
+		size_t			write(uint8_t data);
+		size_t			write(const char *data);
+		size_t			write(const uint8_t *data, size_t quantity);
+		uint8_t			endTransmission(void);
+		uint8_t			endTransmission(uint8_t sendStop);
+		uint8_t			requestFrom(uint8_t address, uint8_t quantity);
+		uint8_t			requestFrom(uint8_t address, uint8_t quantity, uint8_t sendStop);
+		uint8_t			available(void);
+		uint8_t			read(void);
+		void			enableTS(uint8_t on_off);//Не сделанно еще
+		void			setClock(uint32_t clock);
+		void			end(void);
 
 	private:
 		const ZunoWireI2CTypeConfig_t		*_i2c_config;
 		uint32_t							_freqScl;//To set the bus frequency, only valid in master mode - setClock
 		I2C_TransferReturn_TypeDef			seq_return;//Status after reading and writing data
 		uint8_t								available_bytes;//How many bytes read  - requestFrom
-		uint8_t								seq_buffer[WIRE_BUFFER_LENGTH_];//The buffer for reading and writing the size depends on BUFFER_LENGTH
 		I2C_TransferSeq_TypeDef				seq;
+		uint16_t							_doubleBuffer_len;
 };
 
 extern TwoWire Wire;

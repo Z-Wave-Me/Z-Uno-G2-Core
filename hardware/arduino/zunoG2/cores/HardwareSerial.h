41ad79e00ccb39feaabc98f3c6fe6d6e6b2f5dcf
amatilda <kpk@z-wave.me>
2021-01-17 23:49:30 +0300

    printf -> your printf - beta

--- hardware/arduino/zunoG2/cores/HardwareSerial.h
+++ hardware/arduino/zunoG2/cores/HardwareSerial.h
@@ -31,7 +31,7 @@ class HardwareSerial : public Stream {
 		ZunoError_t								begin(size_t baudrate, uint8_t rx, uint8_t tx);
 		ZunoError_t								begin(size_t baudrate, uint8_t rx, uint8_t tx, void *b, hardware_serial_buffer_len len);
 		void									end();
-		ssize_t									printf(const char *format, ...);
+		ssize_t									printf(const char *format, ...)  __attribute__ ((__format__ (__printf__, 2, 3)));
 		uint8_t									write(unsigned long value);
 		uint8_t									write(long value);
 		uint8_t									write(unsigned int value);

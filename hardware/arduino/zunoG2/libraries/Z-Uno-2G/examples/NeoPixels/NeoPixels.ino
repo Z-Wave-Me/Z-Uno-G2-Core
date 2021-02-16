23ddd4c8176088164a032f4b2f116a4c5b15c34a
amatilda <kpk@z-wave.me>
2021-02-16 22:25:18 +0300

    add: example NeoPixels

---
+++ hardware/arduino/zunoG2/libraries/Z-Uno-2G/examples/NeoPixels/NeoPixels.ino
@@ -0,0 +1,37 @@
+#include "Arduino.h"
+#include "ZUNO_NeoPixel.h"
+
+/*
+ * Demo sketch for NeoPxel library for Z-Uno
+ * (c) Z-Wave.Me 2017 for Z-Uno project
+ * some ideas was used from Adafruit library 
+ */
+
+#define MY_SERIAL Serial0
+
+/* LED counter */
+#define LED_COUNT				100
+
+/* LED pin number */
+#define LED_PIN					9
+
+/* the setup function runs once, when you press reset or power the board */
+void setup() {
+	MY_SERIAL.begin();
+	MY_SERIAL.println("Setup");
+	Neo.addNeo(LED_PIN, LED_COUNT);
+}
+
+/* the loop function runs over and over again forever */
+void loop() {
+	/* Turn on all LEDs */
+	Neo.fill(LED_PIN, 0, LED_COUNT, Neo.RGB(255, 255, 0));
+	/* Transferring colors to the ribbon */
+	Neo.show(LED_PIN); 
+	delay(500); 
+	/* Turn off all LEDs */
+	Neo.clear(LED_PIN);
+	/* Transferring colors to the ribbon */
+	Neo.show(LED_PIN); 
+	delay(500); 
+}

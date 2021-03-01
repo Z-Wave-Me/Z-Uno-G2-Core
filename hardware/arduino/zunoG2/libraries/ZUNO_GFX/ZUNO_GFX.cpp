592d7e6e342415fadf9f3d881be31f820c25b6e2
MagistrDev <hks@z-wave.me>
2021-03-01 19:21:16 +0300

    SSD1306 work at 80%

--- hardware/arduino/zunoG2/libraries/ZUNO_GFX/ZUNO_GFX.cpp
+++ hardware/arduino/zunoG2/libraries/ZUNO_GFX/ZUNO_GFX.cpp
@@ -13,6 +13,8 @@
 	}
 #endif
 
+
+
 ZUNO_GFX::ZUNO_GFX(uint16_t width, uint16_t height)
 {
 	font.font_buf = NULL;
@@ -30,21 +32,20 @@ ZUNO_GFX::~ZUNO_GFX()
 
 void ZUNO_GFX::drawPixel(int16_t x, int16_t y, uint8_t color)
 {
-	Serial.println("!!!!!!");
-	if (x >= s_width || y >= s_height)
+	if (x >= s_width || y >= s_height || x < 0 || y < 0)
 		return;
 	uint8_t cur_byte = (y * s_width + x) >> 3;
-	uint8_t cur_bit = (y * s_width + x) % 8;
+	uint8_t cur_bit = 0x01 << ((y * s_width + x) % 8);
 	switch (color)
 	{
 	case 0:
-		buffer[cur_byte] &= (0xff ^ (0x80 >> cur_bit));
+		buffer[cur_byte] &= (0xff ^ cur_bit);
 		break;
 	case 1:
-		buffer[cur_byte] ^= (0x80 >> cur_bit);
+		buffer[cur_byte] ^= cur_bit;
 		break;
 	default:
-		buffer[cur_byte] |= (0x80 >> cur_bit);
+		buffer[cur_byte] |= cur_bit;
 		break;
 	}
 }
@@ -73,20 +74,24 @@ void ZUNO_GFX::drawBitmap(uint16_t x, uint16_t y,uint8_t w, uint8_t h,
 
 void ZUNO_GFX::printChar(uint8_t ch)
 {
+	// Serial.printf("\tchar '%c' / cur_x %d",ch, cur_x);
 	ch -= font.start_sim;
 	uint8_t *buf = font.font_buf + ((font.width + 1) * ch);
 	uint8_t w_sim = *buf;
 	uint8_t *sim = buf + 1;
+	// Serial.printf(" / w_sim %d\n", w_sim);
 	for (uint8_t col = 0; col < w_sim; col++)
 	{
 		for (uint8_t row = 0; row < font.height; row++)
 		{
 			uint8_t cur_b = col * ((font.height >> 3) + (row >> 8));
-			uint8_t cur_bit = 0x80 >> (row % 8);
+			uint8_t cur_bit = 1 << (row % 8);
 			if (sim[cur_b] & cur_bit)
 				drawPixel(cur_x + col, cur_y + row, 2);
 		}
 	}
+	// Serial.printf("last cur %d | Ch %X-'%c' %d\n",cur_x, ch + font.start_sim, ch + font.start_sim, w_sim);
+	cur_x += w_sim;
 }
 
 void	ZUNO_GFX::setFont(uint8_t *font_name)
@@ -116,40 +121,27 @@ uint8_t ZUNO_GFX::write(uint8_t c)
 	if (c >= font.start_sim)
 	{
 		printChar(c);
-		cur_x += w_sim;
+		// cur_x += w_sim;
 	}
 	return (1);
 }
 
 size_t ZUNO_GFX::write(const uint8_t *buf, size_t size)
 {
-	// size_t col_sim = 0;
-	// Serial.printf("'%s|%d'\n", buf, size);
-	// // for(size_t i = 0; i < 1; i++)
-	// {
-		int i = 0;
-	// 	Serial.printf("%d.\t",i);
-	// 	if (buf[i] < 128)
-	// 	{
-	// 		println(" ASCII");
+	size_t col_sim = 0;
+	for(size_t i = 0; i < size; i++)
+	{
+		if (buf[i] < 128)
 			write(buf[i]);
-			write(buf[i+1]);
+		else if (buf[i] & 0xd0)
+			write(buf[++i] - 16);
+		else if (buf[i] & 0xd1)
+			write(buf[++i] + 48);
+		else
+			break;
+		col_sim++;
 
-	// 	}
-	// 	else if (buf[i] & 0xd0)
-	// 	{
-	// 		// write(buf[++i] - 16);
-	// 		println(" D0");
-	// 	}
-	// 	else if (buf[i] & 0xd1)
-	// 	{
-	// 		// write(buf[++i] + 48);
-	// 		println(" D0");
-	// 	}
-	// 	// else
-	// 	// 	break;
-	// 	col_sim++;
-	// }
+	}
 	return(1);
 }
 
@@ -165,7 +157,7 @@ size_t ZUNO_GFX::write(const uint8_t *buf, size_t size)
 /**************************************************************************/
 void ZUNO_GFX::writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
 							 uint8_t color) {
-	Serial.printf("writeLine pt0(%d, %d) pt1(%d, %d)\n",x0,y0,x1,y1);
+	// Serial.printf("writeLine pt0(%d, %d) pt1(%d, %d)\n",x0,y0,x1,y1);
 	int16_t steep = abs(y1 - y0) > abs(x1 - x0);
 	if (steep) {
 	_swap_int16_t(x0, y0);
@@ -176,7 +168,7 @@ void ZUNO_GFX::writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
 	_swap_int16_t(x0, x1);
 	_swap_int16_t(y0, y1);
 	}
-	Serial.printf("writeLine pt0(%d, %d) pt1(%d, %d)\n",x0,y0,x1,y1);
+	// Serial.printf("writeLine pt0(%d, %d) pt1(%d, %d)\n",x0,y0,x1,y1);
 
 	int16_t dx, dy;
 	dx = x1 - x0;
@@ -383,5 +375,185 @@ void ZUNO_GFX::fillCircleHelper(int16_t x0, int16_t y0, int16_t r,
       py = y;
     }
     px = x;
+  }
+}
+
+	void ZUNO_GFX::drawRect(int16_t x, int16_t y, int16_t w, int16_t h,
+															uint16_t color) {
+  writeFastHLine(x, y, w, color);
+  writeFastHLine(x, y + h - 1, w, color);
+  writeFastVLine(x, y, h, color);
+  writeFastVLine(x + w - 1, y, h, color);
+}
+
+/**************************************************************************/
+/*!
+   @brief    Draw a line
+    @param    x0  Start point x coordinate
+    @param    y0  Start point y coordinate
+    @param    x1  End point x coordinate
+    @param    y1  End point y coordinate
+    @param    color 16-bit 5-6-5 Color to draw with
+*/
+/**************************************************************************/
+void ZUNO_GFX::drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
+																uint16_t color) {
+  // Update in subclasses if desired!
+  if (x0 == x1) {
+    if (y0 > y1)
+      _swap_int16_t(y0, y1);
+    drawFastVLine(x0, y0, y1 - y0 + 1, color);
+  } else if (y0 == y1) {
+    if (x0 > x1)
+      _swap_int16_t(x0, x1);
+    drawFastHLine(x0, y0, x1 - x0 + 1, color);
+  } else {
+    writeLine(x0, y0, x1, y1, color);
+  }
+}
+
+/**************************************************************************/
+/*!
+   @brief   Draw a rounded rectangle with no fill color
+    @param    x   Top left corner x coordinate
+    @param    y   Top left corner y coordinate
+    @param    w   Width in pixels
+    @param    h   Height in pixels
+    @param    r   Radius of corner rounding
+    @param    color 16-bit 5-6-5 Color to draw with
+*/
+/**************************************************************************/
+void ZUNO_GFX::drawRoundRect(int16_t x, int16_t y, int16_t w, int16_t h,
+                                 int16_t r, uint16_t color) {
+  int16_t max_radius = ((w < h) ? w : h) / 2; // 1/2 minor axis
+  if (r > max_radius)
+    r = max_radius;
+  // smarter version
+  writeFastHLine(x + r, y, w - 2 * r, color);         // Top
+  writeFastHLine(x + r, y + h - 1, w - 2 * r, color); // Bottom
+  writeFastVLine(x, y + r, h - 2 * r, color);         // Left
+  writeFastVLine(x + w - 1, y + r, h - 2 * r, color); // Right
+  // draw four corners
+  drawCircleHelper(x + r, y + r, r, 1, color);
+  drawCircleHelper(x + w - r - 1, y + r, r, 2, color);
+  drawCircleHelper(x + w - r - 1, y + h - r - 1, r, 4, color);
+  drawCircleHelper(x + r, y + h - r - 1, r, 8, color);
+}
+
+void ZUNO_GFX::writeFillRect(int16_t x, int16_t y, int16_t w, int16_t h,
+															uint16_t color) {
+  // Overwrite in subclasses if desired!
+  fillRect(x, y, w, h, color);
+}
+
+void ZUNO_GFX::fillRoundRect(int16_t x, int16_t y, int16_t w, int16_t h,
+													int16_t r, uint16_t color) {
+  int16_t max_radius = ((w < h) ? w : h) / 2; // 1/2 minor axis
+  if (r > max_radius)
+    r = max_radius;
+  // smarter version
+  writeFillRect(x + r, y, w - 2 * r, h, color);
+  // draw four corners
+  fillCircleHelper(x + w - r - 1, y + r, r, 1, h - 2 * r - 1, color);
+  fillCircleHelper(x + r, y + r, r, 2, h - 2 * r - 1, color);
+}
+
+void ZUNO_GFX::drawTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
+									int16_t x2, int16_t y2, uint16_t color) {
+  drawLine(x0, y0, x1, y1, color);
+  drawLine(x1, y1, x2, y2, color);
+  drawLine(x2, y2, x0, y0, color);
+}
+
+/**************************************************************************/
+/*!
+   @brief     Draw a triangle with color-fill
+    @param    x0  Vertex #0 x coordinate
+    @param    y0  Vertex #0 y coordinate
+    @param    x1  Vertex #1 x coordinate
+    @param    y1  Vertex #1 y coordinate
+    @param    x2  Vertex #2 x coordinate
+    @param    y2  Vertex #2 y coordinate
+    @param    color 16-bit 5-6-5 Color to fill/draw with
+*/
+/**************************************************************************/
+void ZUNO_GFX::fillTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
+                                int16_t x2, int16_t y2, uint16_t color) {
+
+  int16_t a, b, y, last;
+
+  // Sort coordinates by Y order (y2 >= y1 >= y0)
+  if (y0 > y1) {
+    _swap_int16_t(y0, y1);
+    _swap_int16_t(x0, x1);
+  }
+  if (y1 > y2) {
+    _swap_int16_t(y2, y1);
+    _swap_int16_t(x2, x1);
+  }
+  if (y0 > y1) {
+    _swap_int16_t(y0, y1);
+    _swap_int16_t(x0, x1);
+  }
+
+  if (y0 == y2) { // Handle awkward all-on-same-line case as its own thing
+    a = b = x0;
+    if (x1 < a)
+      a = x1;
+    else if (x1 > b)
+      b = x1;
+    if (x2 < a)
+      a = x2;
+    else if (x2 > b)
+      b = x2;
+    writeFastHLine(a, y0, b - a + 1, color);
+    return;
+  }
+
+  int16_t dx01 = x1 - x0, dy01 = y1 - y0, dx02 = x2 - x0, dy02 = y2 - y0,
+          dx12 = x2 - x1, dy12 = y2 - y1;
+  int32_t sa = 0, sb = 0;
+
+  // For upper part of triangle, find scanline crossings for segments
+  // 0-1 and 0-2.  If y1=y2 (flat-bottomed triangle), the scanline y1
+  // is included here (and second loop will be skipped, avoiding a /0
+  // error there), otherwise scanline y1 is skipped here and handled
+  // in the second loop...which also avoids a /0 error here if y0=y1
+  // (flat-topped triangle).
+  if (y1 == y2)
+    last = y1; // Include y1 scanline
+  else
+    last = y1 - 1; // Skip it
+
+  for (y = y0; y <= last; y++) {
+    a = x0 + sa / dy01;
+    b = x0 + sb / dy02;
+    sa += dx01;
+    sb += dx02;
+    /* longhand:
+    a = x0 + (x1 - x0) * (y - y0) / (y1 - y0);
+    b = x0 + (x2 - x0) * (y - y0) / (y2 - y0);
+    */
+    if (a > b)
+      _swap_int16_t(a, b);
+    writeFastHLine(a, y, b - a + 1, color);
+  }
+
+  // For lower part of triangle, find scanline crossings for segments
+  // 0-2 and 1-2.  This loop is skipped if y1=y2.
+  sa = (int32_t)dx12 * (y - y1);
+  sb = (int32_t)dx02 * (y - y0);
+  for (; y <= y2; y++) {
+    a = x1 + sa / dy12;
+    b = x0 + sb / dy02;
+    sa += dx12;
+    sb += dx02;
+    /* longhand:
+    a = x1 + (x2 - x1) * (y - y1) / (y2 - y1);
+    b = x0 + (x2 - x0) * (y - y0) / (y2 - y0);
+    */
+    if (a > b)
+      _swap_int16_t(a, b);
+    writeFastHLine(a, y, b - a + 1, color);
   }
 }
--- hardware/arduino/zunoG2/libraries/ZUNO_GFX/ZUNO_GFX.h
+++ hardware/arduino/zunoG2/libraries/ZUNO_GFX/ZUNO_GFX.h
@@ -11,6 +11,8 @@ typedef struct GLCDfonts_s
 	uint8_t		*font_buf;
 }				GLCDfonts;
 
+#define max(a, b) (((a) > (b)) ? (a) : (b))
+
 class ZUNO_GFX : public Print
 {
 	protected:
@@ -20,11 +22,11 @@ class ZUNO_GFX : public Print
 		uint16_t	s_height;	
 		GLCDfonts	font;
 		void printChar(uint8_t ch);
+		uint8_t		*buffer;
+		size_t		buff_size;
 		
 	private:
 	public:
-		uint8_t		*buffer;
-		size_t		buff_size;
 		ZUNO_GFX(uint16_t width, uint16_t height);
 		~ZUNO_GFX();
 		using	Print::write;
@@ -35,6 +37,8 @@ class ZUNO_GFX : public Print
 		void drawBitmap(uint16_t x, uint16_t y,uint8_t w, uint8_t h,
 												 uint8_t *pic, uint8_t color);
 		void writeLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint8_t color);
+		void drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, 
+																uint16_t color);
 		void writeFastVLine(int16_t x, int16_t y, int16_t h, uint8_t color);
 		void drawFastVLine(int16_t x, int16_t y, int16_t h, uint8_t color);
 		void writeFastHLine(int16_t x, int16_t y, int16_t w, uint8_t color);
@@ -42,12 +46,23 @@ class ZUNO_GFX : public Print
 		void fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint8_t color);
 		void fillScreen(uint8_t color);
 		void drawCircle(int16_t x0, int16_t y0, int16_t r, uint8_t color);
+		void drawRoundRect(int16_t x, int16_t y, int16_t w, int16_t h,
+													int16_t r, uint16_t color);
 		void drawCircleHelper(int16_t x0, int16_t y0, int16_t r,
 											uint8_t cornername, uint8_t color);
+		void fillRoundRect(int16_t x, int16_t y, int16_t w, int16_t h,
+													int16_t r, uint16_t color);
+		void writeFillRect(int16_t x, int16_t y, int16_t w, int16_t h,
+															uint16_t color);
 		void fillCircle(int16_t x0, int16_t y0, int16_t r, uint8_t color);
 		void fillCircleHelper(int16_t x0, int16_t y0, int16_t r,
 								uint8_t corners, int16_t delta, uint8_t color);
-		void	setFont(uint8_t *font_name);
+		void drawRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color);
+		void setFont(uint8_t *font_name);
+		void drawTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
+									int16_t x2, int16_t y2, uint16_t color);
+		void fillTriangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1,
+										int16_t x2, int16_t y2, uint16_t color);
 		void	clearDisp() {memset(buffer, 0x00, buff_size);};
 		int16_t	width(void) const { return s_width; };
 		int16_t	height(void) const { return s_height; };

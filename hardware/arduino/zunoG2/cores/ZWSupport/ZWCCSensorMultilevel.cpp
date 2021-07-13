087cff9510770fd9aa3c807426b3af0e0637de6c
amatilda <kpk@z-wave.me>
2021-07-13 12:01:07 +0300

    fix:ZWCCSensorMultilevel

--- hardware/arduino/zunoG2/cores/ZWSupport/ZWCCSensorMultilevel.cpp
+++ hardware/arduino/zunoG2/cores/ZWSupport/ZWCCSensorMultilevel.cpp
@@ -24,20 +24,26 @@ int zuno_CCSensorMultilevelReport(byte channel, bool reply) {
 	return (ZUNO_COMMAND_ANSWERED);
 }
 
-static int _supported_scale(ZwSensorMultilevelSupportedGetScaleFrame_t *cmd, size_t channel) {
+static int _supported_scale(ZwSensorMultilevelSupportedGetScaleFrame_t *cmd) {
 	ZwSensorMultilevelSupportedScaleReportFrame_t				*report;
 	size_t														sensorType;
 	size_t														properties1;
+	size_t														i;
 
 	report = (ZwSensorMultilevelSupportedScaleReportFrame_t *)&CMD_REPLY_CC;
 	sensorType = cmd->sensorType;
 	report->cmdClass = COMMAND_CLASS_SENSOR_MULTILEVEL;
 	report->cmd = SENSOR_MULTILEVEL_SUPPORTED_SCALE_REPORT;
 	report->sensorType = sensorType;
-	if (ZUNO_CFG_CHANNEL(channel).sub_type == sensorType)
-		properties1 = 1 << ((ZUNO_CFG_CHANNEL(channel).params[0] & SENSOR_MULTILEVEL_PROPERTIES_SCALE_MASK) >> SENSOR_MULTILEVEL_PROPERTIES_SCALE_SHIFT);
-	else
-		properties1 = 0;
+	i = 0x0;
+	properties1 = 0x0;
+	while (i < ZUNO_CFG_CHANNEL_COUNT) {
+		if (ZUNO_CFG_CHANNEL(i).type == ZUNO_SENSOR_MULTILEVEL_CHANNEL_NUMBER && ZUNO_CFG_CHANNEL(i).sub_type == sensorType) {
+			properties1 = 1 << ((ZUNO_CFG_CHANNEL(i).params[0] & SENSOR_MULTILEVEL_PROPERTIES_SCALE_MASK) >> SENSOR_MULTILEVEL_PROPERTIES_SCALE_SHIFT);
+			break ;
+		}
+		i++;
+	}
 	report->properties1 = properties1;
 	CMD_REPLY_LEN = sizeof(ZwSensorMultilevelSupportedScaleReportFrame_t);
 	return (ZUNO_COMMAND_ANSWERED);
@@ -49,8 +55,8 @@ static int _supported_sensor(ZUNOCommandPacket_t *pack, size_t channel) {
 
 	report = (ZwSensorMultilevelSupportedSensorReportFrame_t *)&CMD_REPLY_CC;
 	memset(&report->bitMask[0], 0, SENSOR_MULTILEVEL_SUPPORTED_MAX_BYTE_MASK);// clear reply bit mask
-	report->cmdClass = COMMAND_CLASS_SENSOR_MULTILEVEL;
-	report->cmd = SENSOR_MULTILEVEL_SUPPORTED_SENSOR_REPORT;
+	// report->cmdClass = COMMAND_CLASS_SENSOR_MULTILEVEL; set in - fillOutgoingPacket
+	// report->cmd = SENSOR_MULTILEVEL_SUPPORTED_SENSOR_REPORT; set in - fillOutgoingPacket
 	if(pack->dst_zw_channel == 0) {// In case it's a 0 channel mapping we have to declare all the types.
 		for (i = 0; i < ZUNO_CFG_CHANNEL_COUNT; i++){
 			if (ZUNO_CFG_CHANNEL(i).type != ZUNO_SENSOR_MULTILEVEL_CHANNEL_NUMBER)
@@ -101,7 +107,7 @@ int zuno_CCSensorMultilevelHandler(byte channel, ZUNOCommandPacket_t *cmd) {
 			rs = _supported_sensor(cmd, channel);
 			break;
 		case SENSOR_MULTILEVEL_SUPPORTED_GET_SCALE:
-			rs = _supported_scale((ZwSensorMultilevelSupportedGetScaleFrame_t *)cmd->cmd, channel);
+			rs = _supported_scale((ZwSensorMultilevelSupportedGetScaleFrame_t *)cmd->cmd);
 			break;
 		default:
 			rs = ZUNO_UNKNOWN_CMD;

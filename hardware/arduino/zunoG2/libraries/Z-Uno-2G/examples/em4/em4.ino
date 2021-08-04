72e3e01033d0550bffa965f8d5854b9c9718c8c6
amatilda <kpk@z-wave.me>
2021-08-04 21:57:57 +0300

    update em4

--- hardware/arduino/zunoG2/libraries/Z-Uno-2G/examples/em4/em4.ino
+++ hardware/arduino/zunoG2/libraries/Z-Uno-2G/examples/em4/em4.ino
@@ -2,20 +2,50 @@
 
 #define MODEM_WUP_PIN					23//Btn
 
-void setup() {
-	pinMode(MODEM_WUP_PIN, INPUT_PULLUP_FILTER);
-	zunoAttachSysHandler(ZUNO_HANDLER_SLEEP, 0, (void*) &_sleepHandler);
-	zunoSetSleepingMode(ZUNO_SLEEPING_MODE_SLEEPING);
-	zunoEnableSmartStart(false);
-	zunoCommitCfg();
+ZUNO_ENABLE(WITH_CC_BATTERY LOGGING_DBG LOGGING_UART=Serial0);
+uint32_t int_cnt=0;
+uint32_t last_wake_time = 0;
+void pinWakeInt(){
+	int_cnt++;
 }
 
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, 1);
+    Serial0.begin();
+    Serial0.print("START");
+	pinMode(MODEM_WUP_PIN, INPUT_PULLUP_FILTER);
+    attachInterrupt(MODEM_WUP_PIN, &pinWakeInt, FALLING);
+	zunoAttachSysHandler(ZUNO_HANDLER_SLEEP, 0, (void*) &_sleepHandler);
+    zunoAttachSysHandler(ZUNO_HANDLER_WUP, 0, (void*) &_wakeHandler);
+    
+    if(zunoStartDeviceConfiguration()) {
+        zunoSetSleepingMode(ZUNO_SLEEPING_MODE_SLEEPING);
+        zunoCommitCfg();
+    }
+    last_wake_time = millis();
+	zunoSetSleepingMode(ZUNO_SLEEPING_MODE_SLEEPING);
+
+}
+void _wakeHandler(void){
+    digitalWrite(LED_BUILTIN, 1);
+    last_wake_time = millis();
+}
 void _sleepHandler(void){
+    digitalWrite(LED_BUILTIN, 0);
 	zunoEM4EnablePinWakeup(MODEM_WUP_PIN);
 }
 
 
 void loop() {
-	if (millis() > 5000)
-		zunoSendDeviceToSleep();	
+    Serial0.print("millis:");
+    Serial0.print(millis());
+    Serial0.print(" last wake:");
+    Serial0.print(last_wake_time);
+    Serial0.print(" ints:");
+    Serial0.println(int_cnt);
+
+	if ((millis() - last_wake_time) > 5000)
+		zunoSendDeviceToSleep();
+    	
 }

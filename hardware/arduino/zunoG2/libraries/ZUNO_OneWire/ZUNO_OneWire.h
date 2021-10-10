4cda392b72bf998f58f3d85b512c9058e8d2db8e
amatilda <kpk@z-wave.me>
2021-10-10 18:33:45 +0300

    OneWire

--- hardware/arduino/zunoG2/libraries/ZUNO_OneWire/ZUNO_OneWire.h
+++ hardware/arduino/zunoG2/libraries/ZUNO_OneWire/ZUNO_OneWire.h
@@ -15,13 +15,17 @@ class OneWire {
 		void						reset_search();
 		byte						reset(void);
 		void						write(byte v, byte power);
-		inline void					write(byte v) {this->write(v, true);};
+		inline void					write(byte v) {this->write(v, false);};
+		void						write_bytes(const uint8_t *buf, uint16_t count, bool power = 0);
 		byte						read();
+		void						read_bytes(uint8_t *buf, uint16_t count);
 		void						select(byte * rom);
 		inline void					skip() {this->write(0xCC);}; // Skip ROM
 		void						depower();
 		void						readROM(byte * rom);
 		static byte					crc8(byte *addr, byte len);
+		static bool					check_crc16(const uint8_t* input, uint16_t len, const uint8_t* inverted_crc, uint16_t crc = 0);
+		static uint16_t				crc16(const uint8_t* input, uint16_t len, uint16_t crc = 0);
 		bool						search(uint8_t * newAddr);
 
 	private:

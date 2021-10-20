8a15f8e1e69e2bc4a737c3c5213356bbbf241d19
amatilda <kpk@z-wave.me>
2021-10-20 15:05:28 +0300

    update one wire

--- hardware/arduino/zunoG2/libraries/ZUNO_OneWire/ZUNO_OneWire.h
+++ hardware/arduino/zunoG2/libraries/ZUNO_OneWire/ZUNO_OneWire.h
@@ -20,20 +20,22 @@ class OneWire {
 		byte						read();
 		void						read_bytes(uint8_t *buf, uint16_t count);
 		void						select(byte * rom);
+		void						select(const uint8_t *rom) {this->select((uint8_t *)rom);};
 		inline void					skip() {this->write(0xCC);}; // Skip ROM
 		void						depower();
 		void						readROM(byte * rom);
 		static byte					crc8(byte *addr, byte len);
+		static byte					crc8(const uint8_t *addr, byte len) {return (OneWire::crc8((uint8_t *)addr, len));};
 		static bool					check_crc16(const uint8_t* input, uint16_t len, const uint8_t* inverted_crc, uint16_t crc = 0);
 		static uint16_t				crc16(const uint8_t* input, uint16_t len, uint16_t crc = 0);
 		bool						search(uint8_t * newAddr);
+		void						write_bit(bool bit);
+		bool						read_bit();
 
 	private:
 		inline void						_digitalWriteFast(uint8_t val);
 		inline size_t					_digitalReadFast();
 		inline void						_pinModeFast(uint8_t);
-		inline bool						_read_bit();
-		inline void						_write_bit(bool bit);
 		size_t							_aliasAddr_in;
 		size_t							_aliasAddr_out;
 		uint8_t							_real_port;

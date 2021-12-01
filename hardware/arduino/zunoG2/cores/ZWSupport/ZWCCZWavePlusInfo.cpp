1adc661373574cce2f029e70bb2c344d05dedf25
Alexander Polyakov <polyakov@z-wave.me>
2021-12-01 16:00:02 +0300

    Fixed AGI icon type. Fixed AssociationKey detection during node remove.

--- hardware/arduino/zunoG2/cores/ZWSupport/ZWCCZWavePlusInfo.cpp
+++ hardware/arduino/zunoG2/cores/ZWSupport/ZWCCZWavePlusInfo.cpp
@@ -2,12 +2,15 @@
 #include "ZWSupport.h"
 #include "ZWCCZWavePlusInfo.h"
 
+
 static int _report(ZUNOCommandPacket_t *cmd) {
 	ZwZwavePlusInfoReportFrame_t		*report;
 	size_t								installerIconType;
 	size_t								userIconType;
 	size_t								roleType;
 
+	ZUNOChannel_t * ch_data =  zuno_findChannelByZWChannel(zw_channel);
+	uint8_t type_index = ch_data->type -1;
 	report = (ZwZwavePlusInfoReportFrame_t *)&CMD_REPLY_CC;
 	report->v2.cmdClass = COMMAND_CLASS_ZWAVEPLUS_INFO;
 	report->v2.cmd = ZWAVEPLUS_INFO_REPORT;
@@ -25,8 +28,8 @@ static int _report(ZUNOCommandPacket_t *cmd) {
 	}
 	report->v2.roleType = roleType;
 	report->v2.nodeType = ZWAVEPLUS_INFO_REPORT_NODE_TYPE_ZWAVEPLUS_NODE;
-	installerIconType = ZUNO_DEV_TYPES[cmd->dst_zw_channel].icon;
-	userIconType = ZUNO_DEV_TYPES[cmd->dst_zw_channel].app_icon;
+	installerIconType = ZUNO_DEV_TYPES[type_index].icon;
+	userIconType = ZUNO_DEV_TYPES[type_index].app_icon;
 	report->v2.installerIconType1 = installerIconType >> 8;
 	report->v2.installerIconType2 = installerIconType & 0xFF;
 	report->v2.userIconType1 = userIconType >> 8;

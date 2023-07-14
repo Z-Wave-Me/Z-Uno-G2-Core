a5ff1ec8c200634b9855b62a488499438fb8dea5
amatilda <kpk@z-wave.me>
2023-07-15 02:22:03 +0300

    update example meter

--- hardware/arduino/zunoG2/libraries/Z-Uno-2G/examples/05-CmdClass/Meter/Meter.ino
+++ hardware/arduino/zunoG2/libraries/Z-Uno-2G/examples/05-CmdClass/Meter/Meter.ino
@@ -25,7 +25,7 @@ ZUNO_SETUP_CHANNELS(
 	ZUNO_METER(ZUNO_METER_TYPE_WATER, METER_RESET_DISABLE, ZUNO_METER_WATER_SCALE_PULSECOUNT, METER_SIZE_ONE_BYTE, METER_PRECISION_ZERO_DECIMALS, ft_get_water, ft_empty),
 	ZUNO_METER(ZUNO_METER_TYPE_HEATING, METER_RESET_DISABLE, ZUNO_METER_HEATING_SCALE_KWH, METER_SIZE_FOUR_BYTES, METER_PRECISION_TWO_DECIMALS, ft_get_heating, ft_empty),
 	ZUNO_METER(ZUNO_METER_TYPE_COOLING, METER_RESET_DISABLE, ZUNO_METER_COOLING_SCALE_KWH, METER_SIZE_FOUR_BYTES, METER_PRECISION_TWO_DECIMALS, ft_get_cooling, ft_empty),
-	ZUNO_METER(ZUNO_METER_TYPE_ELECTRIC, METER_RESET_ENABLE, ZUNO_METER_ELECTRIC_SCALE_KWH, METER_SIZE_TWO_BYTES, METER_PRECISION_ONE_DECIMAL, ft_get_kvar, ft_get_kvar_reset)
+	ZUNO_METER(ZUNO_METER_TYPE_ELECTRIC, METER_RESET_ENABLE, ZUNO_METER_ELECTRIC_SCALE_KVAR, METER_SIZE_TWO_BYTES, METER_PRECISION_ONE_DECIMAL, ft_get_kvar, ft_get_kvar_reset)
 );
 
 static uint32_t _kwh = 0x0;
@@ -36,23 +36,29 @@ static uint32_t _cooling = 0x0;
 static uint16_t _kvar = 0x0;
 
 void setup(void) {
-
+	_kwh = random(300, 10000);
+	_gas = random(100, 500);
+	_water = random(1, 255);
+	_heating = random(1000, 5555);
+	_cooling = random(3000, 44000);
+	_kvar = random(333, 11111);
 }
 
 void loop(void) {
-	_kwh = random(300, 10000);
+	delay(3000);
+	_kwh++;
+	_gas++;
+	_water++;
+	_heating++;
+	_cooling++;
+	_kvar++;
 	zunoSendReport(INDEX_ELECTRIC_KHW);
-	_gas = random(100, 500);
 	zunoSendReport(INDEX_GAS);
-	_water = random(1, 255);
 	zunoSendReport(INDEX_WATER);
-	_heating = random(1000, 5555);
 	zunoSendReport(INDEX_HEATING);
-	_cooling = random(3000, 44000);
 	zunoSendReport(INDEX_COOLING);
-	_kvar = random(333, 11111);
 	zunoSendReport(INDEX_ELECTRIC_KVAR);
-	delay(1000);
+	
 }
 
 void ft_get_kwh_reset(uint32_t value)

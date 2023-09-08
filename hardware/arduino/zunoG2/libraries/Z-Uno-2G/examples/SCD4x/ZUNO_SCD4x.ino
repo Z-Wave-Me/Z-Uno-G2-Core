615894def7258b83b512693dc6e70ea32da12cf7
Michael P <contact@smarthome-work.com>
2023-09-08 13:50:27 +0200

    add ZUNO_SCD4x lib / example

---
+++ hardware/arduino/zunoG2/libraries/Z-Uno-2G/examples/SCD4x/ZUNO_SCD4x.ino
@@ -0,0 +1,150 @@
+#include "Wire.h"
+#include "ZUNO_SCD4x.h"
+uint16_t Address = 0x62;
+uint8_t cnt = 0;
+int16_t lastTemp = 0;
+int16_t curTemp;
+uint16_t curHum, curCO2;
+uint16_t lastHum = 0;
+uint16_t lastCO2 = 10;
+
+enum{
+    PARAM_TEMP_OFFSET=64,
+    PARAM_TEMP_HYST,
+    PARAM_HUMIDITY_HYST,
+    PARAM_CO2_HYST,
+    PARAM_REPORTING_INT,
+    PARAM_AMB_PRESSURE,
+    PARAM_SENS_ALT,
+    PARAM_FACTORY_RESET
+};
+
+ZUNO_SETUP_SLEEPING_MODE(ZUNO_SLEEPING_MODE_ALWAYS_AWAKE); //Z-Uno Sleaping mode
+ZUNO_SETUP_CHANNELS(
+  ZUNO_SENSOR_MULTILEVEL(ZUNO_SENSOR_MULTILEVEL_TYPE_CO2_LEVEL, SENSOR_MULTILEVEL_SCALE_PARTS_PER_MILLION, SENSOR_MULTILEVEL_SIZE_TWO_BYTES, SENSOR_MULTILEVEL_PRECISION_ZERO_DECIMALS, getterCO2),                                  
+  ZUNO_SENSOR_MULTILEVEL(ZUNO_SENSOR_MULTILEVEL_TYPE_TEMPERATURE, SENSOR_MULTILEVEL_SCALE_CELSIUS, SENSOR_MULTILEVEL_SIZE_TWO_BYTES, SENSOR_MULTILEVEL_PRECISION_ONE_DECIMAL, getterTemp),  
+  ZUNO_SENSOR_MULTILEVEL(ZUNO_SENSOR_MULTILEVEL_TYPE_RELATIVE_HUMIDITY, SENSOR_MULTILEVEL_SCALE_PERCENTAGE_VALUE, SENSOR_MULTILEVEL_SIZE_TWO_BYTES, SENSOR_MULTILEVEL_PRECISION_ONE_DECIMAL,getterHum)
+);
+ ZUNO_SETUP_CONFIGPARAMETERS(
+         ZUNO_CONFIG_PARAMETER_INFO("Temperature offset (step 1/10°C)", "Define temperature offset in 1/10 grad, it can depend on various factors such as the SCD4x measurement mode, self-heating of close components", 0, 200, 45),
+         ZUNO_CONFIG_PARAMETER_INFO("Temperature hysteresis (step 1/10°C)", "Defines hysteresis of temperature for additional sending value in 1/10 grad", 1, 20, 5),
+         ZUNO_CONFIG_PARAMETER_INFO("Humidity hysteresis", "Defines hysteresis of humidity for additional sending value", 1, 20, 5),
+         ZUNO_CONFIG_PARAMETER_INFO("CO2 hysteresis", "Defines hysteresis of CO2 value for additional sending value", 0, 1000, 0),
+         ZUNO_CONFIG_PARAMETER_INFO("Reporting interval (sec)", "Defines the default reporting interval (possible step: 5sec)", 20, 3600, 40),
+         ZUNO_CONFIG_PARAMETER_INFO("Ambient pressure (Pa)", "Note that setting an ambient pressure using 'Set Ambient Pressure' overrides any pressure compensation based on a previously set sensor altitude. Use of this command is highly recommended for applications *experiencing significant ambient pressure changes* to ensure sensor accuracy", 0, 150000, 0),
+         ZUNO_CONFIG_PARAMETER_INFO("Sensor altitude (m)", "Typically, the sensor altitude is set once after device installation", 0, 5000, 0),
+         ZUNO_CONFIG_PARAMETER_INFO("Factory reset", "Set to '1' for factory reset and then Z-Uno is restarting", 0, 1, 0)
+ );
+
+ZUNO_SCD4x SCD4x;
+
+void setup() {
+  float cfgParam1;
+  Wire.begin();  
+  Serial.begin(115200);
+  delay(1000);
+  SCD4x.stopPeriodicMeasurement();           // for reading/setting sensor parameters - see doc
+  
+  if(zunoInNetwork())
+  {
+    zunoSaveCFGParam(PARAM_FACTORY_RESET,0);   // Reset the value to default
+  
+    // Get configuration parameters and set sensor values (must be done before read measurement!)
+    cfgParam1 = zunoLoadCFGParam(PARAM_TEMP_OFFSET)/10.0;
+    bool result = SCD4x.setTemperatureOffset(cfgParam1);
+    cfgParam1 = zunoLoadCFGParam(PARAM_AMB_PRESSURE);
+    result = result && SCD4x.setAmbientPressure(cfgParam1);
+    cfgParam1 = zunoLoadCFGParam(PARAM_SENS_ALT);
+    result = result && SCD4x.setSensorAltitude(cfgParam1);
+    if (result == false) Serial.println("error while setting parameters! Error number: (" + String(SCD4x.comError) + ")");
+
+  }
+  else
+  {
+    Serial.println("Z-UNO IS NOT IN NETWORK INCLUDED - NO Z-WAVE FEATURES AVAILABLE!");
+  }
+  printSensorSettings();
+  bool ready = SCD4x.startPeriodicMeasurement();
+}
+
+void loop() {
+  delay(5000);  // minimum - see doc
+  SCD4x.readMeasurement();
+
+  if(zunoInNetwork())    // Check if Z-Uno is included in Network
+  {
+    cnt++;
+    if(zunoLoadCFGParam(PARAM_REPORTING_INT) <= cnt * 5) cnt = 0;   // define reporting interval
+  
+    // bool result = Serial.println(SCD4x.getDataReadyStatus()) ;  // optional use to be sure about it, integrated crc check will detect wrong values
+    if(SCD4x.comError == 0)
+    {
+      curCO2=SCD4x.CO2;
+      if(abs(lastCO2 - curCO2) >= zunoLoadCFGParam(PARAM_CO2_HYST) || cnt == 0)
+      {
+        zunoSendReport(1);
+        lastCO2 = curCO2;
+      }
+      Serial.print("CO2: ");Serial.println(curCO2);
+      curTemp=10*SCD4x.Temp;
+      if(abs(lastTemp - curTemp) >= 10 * zunoLoadCFGParam(PARAM_TEMP_HYST) || cnt == 0)
+      {
+        zunoSendReport(2);
+        lastTemp = curTemp;
+      }
+      Serial.print("Temp: ");Serial.println(curTemp/10.0);
+      curHum=10*SCD4x.Hum;
+      if(abs(lastHum - curHum) >= 10 * zunoLoadCFGParam(PARAM_HUMIDITY_HYST) || cnt == 0)
+      {
+        zunoSendReport(3);
+        lastHum = curHum;
+      }
+      Serial.print("Hum: ");Serial.println(curHum/10.0);
+
+      if (zunoLoadCFGParam(PARAM_FACTORY_RESET) == 1) 
+      {
+        Serial.println("FACTORY RESET");
+        //SCD4x.perfomFactoryReset();
+        zunoReboot();
+      }
+    }
+    else
+    {
+      Serial.print("communication error: "); Serial.println(SCD4x.comError);
+    }
+  }
+  else        // WITHOUT Z-WAVE NETWORK SIMPLE REQUEST  without error handling
+  {
+      Serial.println("-- NO Z-WAVE network! --");
+      Serial.print("CO2:  ");Serial.println(SCD4x.CO2);
+      Serial.print("Temp: ");Serial.println(SCD4x.Temp);
+      Serial.print("Hum:  ");Serial.println(SCD4x.Hum);
+  }
+}
+
+int getterCO2(void) {
+  return curCO2;
+}
+
+int getterTemp(void) {
+  return (curTemp);
+}
+
+int getterHum(void) {
+  return (curHum);
+}
+
+void printSensorSettings()
+{
+  bool result=SCD4x.getSensorData();
+  Serial.print("Sensor S/N: ");Serial.println(SCD4x.SerialNumber);
+  Serial.print("Temp.-Offset:");Serial.println(SCD4x.TempOffset);
+  Serial.print("Sensor Altitude: ");Serial.println(SCD4x.sensorAlt);
+  Serial.print("Sensor AutomaticSelfCalibration: ");Serial.println(SCD4x.sensorASC);
+  if(zunoInNetwork())
+  {
+    Serial.print("Sensor Ambient Pressure: ");Serial.println(zunoLoadCFGParam(PARAM_AMB_PRESSURE));
+    Serial.print("Sensor Factory Reset after reboot: ");Serial.println(zunoLoadCFGParam(PARAM_FACTORY_RESET));
+  }
+  if (result == false) Serial.println("error while getting sensor parameters!");
+}
---
+++ hardware/arduino/zunoG2/libraries/ZUNO_SCD4x/ZUNO_SCD4x.h
@@ -0,0 +1,89 @@
+/* CO2 sensor SCD4x library for Z-Uno G2
+ *  v. 1.0 07.09.2023  by Michael Pruefer
+ *  based on Sensirion sensor documentation
+ *  see https://sensirion.com/media/documents/E0F04247/631EF271/CD_DS_SCD40_SCD41_Datasheet_D1.pdf
+ *
+ */
+//ZUNO_SCD4x.h
+
+#ifndef ZUNO_SCD4x_h
+#define ZUNO_SCD4x_h
+
+#include "Arduino.h"
+
+#define SCD4x_ADDR  	0x62	// I2C Address
+
+#define SCD4x_CMD_STPM  0x21b1		// cmd start_periodic_measurement
+#define SCD4x_CMD_REAM  0xec05		// cmd read_measurement
+#define SCD4x_CMD_SPPM  0x3f86		// cmd stop_periodic_measurement
+#define SCD4x_CMD_STOF  0x241d		// cmd set_temperature_offset
+#define SCD4x_CMD_GTOF  0x2318		// cmd get_temperature_offset
+#define SCD4x_CMD_SSAL  0x2427		// cmd set_sensor_altitude
+#define SCD4x_CMD_GSAL  0x2322		// cmd get_sensor_altitude
+#define SCD4x_CMD_SAPR  0xe000		// cmd set_ambient_pressure
+#define SCD4x_CMD_PCAL  0x362f		// cmd perform_forced_recalibration
+#define SCD4x_CMD_SACA  0x2416		// cmd set_automatic_self_calibration_enabled
+#define SCD4x_CMD_GACA  0x2313		// cmd get_automatic_self_calibration_enabled
+#define SCD4x_CMD_SLPM  0x21ac		// cmd start_low_power_periodic_measurement
+#define SCD4x_CMD_GDRA  0xe4b8		// cmd get_data_ready_status
+#define SCD4x_CMD_PERS  0x3615		// cmd persist_settings
+#define SCD4x_CMD_GSER  0x3682		// cmd get_serial_number
+#define SCD4x_CMD_PSTE  0x3639		// cmd perform_self_test
+#define SCD4x_CMD_PFRS  0x3632		// cmd perfom_factory_reset
+#define SCD4x_CMD_PRIN  0x3646		// cmd reinit
+//SCD41
+#define SCD4x_CMD_MSSH  0x219d		// cmd measure_single_shot
+#define SCD4x_CMD_MSS1  0x2196		// cmd measure_single_shot_rht_only
+#define SCD4x_CMD_PWDN  0x36e0		// cmd power_down
+#define SCD4x_CMD_WKUP  0x36f6		// cmd wake_up
+
+
+class ZUNO_SCD4x
+{
+  public:
+	ZUNO_SCD4x();
+	bool startPeriodicMeasurement(void);
+	bool stopPeriodicMeasurement(void);
+	void readMeasurement(byte*,int);    // read data
+	void readMeasurement(void);    // read data
+  bool getDataReadyStatus(void);
+  bool setTemperatureOffset(float tempOffset);
+  bool getTemperatureOffset(void);
+  bool setSensorAltitude(uint16_t sensorAlt);
+  bool getSensorAltitude(void);
+  bool setAmbientPressure(uint32_t ambPressure);
+  bool setAutomaticSelfCalibrationEnabled(uint16_t ASCEnabled);
+  bool getAutomaticSelfCalibrationEnabled(void);
+  bool perform_forced_recalibration(uint16_t targetCO2ppm);
+  bool startLowPowerPeriodicMeasurement(void);
+  bool PersistSettings(void);
+  bool getSerialNumber(void);
+  bool perfomFactoryReset(void);
+  bool reinit(void);
+  bool measureSingleShot(void);
+  bool measureSingleShotRHTOnly(void);
+  bool powerDown(void);
+  bool wakeUp(void);
+  bool getSensorData(void);           // read all sensor properties
+
+	// sensor properties
+  uint16_t CO2;
+	float Temp;
+	float Hum;
+  float TempOffset;
+  uint8_t comError;               // 1- I2C error, 2- CRC error
+  uint16_t sensorASC;
+  uint16_t sensorAlt;
+  float CO2Correction;
+  String SerialNumber;
+
+  private:
+	uint16_t crc;
+  bool sendCmd(uint16_t cmdCode, uint16_t delayMs);
+  bool sendCmd(uint16_t cmdCode, uint16_t parameter, uint16_t delayMs);
+  uint16_t readDbyte(uint16_t registerAddress, uint16_t delayMs = 1);
+  uint8_t calcCRC8(uint8_t data[], uint8_t len);
+  void arrayToString(byte array[], uint8_t len, char strBuffer[]);
+};
+
+#endif

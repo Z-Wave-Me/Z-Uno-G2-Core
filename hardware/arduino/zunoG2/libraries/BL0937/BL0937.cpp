c5f97c2c0c62e37e8cfb0e549669e2b77d8475c7
magistrdev <magistrdev@gmail.com>
2024-05-30 20:34:28 +0700

    Add lib BL0937 Watt meter

---
+++ hardware/arduino/zunoG2/libraries/BL0937/BL0937.cpp
@@ -0,0 +1,76 @@
+#include "BL0937.h"
+
+BL0937::BL0937(byte cfPin, byte cf1Pin,byte selPin, float vRef):
+                _cfPin(cfPin),
+                _cf1Pin(cf1Pin),
+                _selPin(selPin),
+                _vRef(vRef),
+                ZMEHandlerMapper(SYS_HANLER_MAPPER_TIMER){
+        };
+
+void BL0937::begin(){
+    unsigned long currentTime = millis();
+    pinMode(_cfPin, INPUT);
+    pinMode(_cf1Pin, INPUT);
+    pinMode(_selPin, OUTPUT);
+
+    _lastMeasurementTime = currentTime;
+    _lastStorageTime = currentTime;
+    
+};
+
+float BL0937::readActivePower() {
+    unsigned long pulseDuration = _readPulseDuration(_cfPin);
+    if (pulseDuration > 0) {
+        float frequency = 1000000.0 / pulseDuration;
+        _activePower = (frequency * _vRef * _vRef) / 1721506.0;
+        return _activePower;
+    }
+    return 0;
+}
+float BL0937::readVoltageRMS() {
+    digitalWrite(_selPin, LOW);
+    delay(10);
+    unsigned long pulseDuration = _readPulseDuration(_cf1Pin);
+    if (pulseDuration > 0) {
+        float frequency = 1000000.0 / pulseDuration;
+        _voltageRMS = (frequency * _vRef) / 15397.0;
+        return _voltageRMS;
+    }
+    return 0;
+}
+float BL0937::readCurrentRMS() {
+    digitalWrite(_selPin, HIGH);
+    delay(10);
+    unsigned long pulseDuration = _readPulseDuration(_cf1Pin);
+    if (pulseDuration > 0) {
+        float frequency = 1000000.0 / pulseDuration;
+        _currentRMS = (frequency * _vRef) / 94638.0;
+        return _currentRMS;
+    }
+    return 0;
+}
+
+unsigned long BL0937::_readPulseDuration(int pin) {
+    // unsigned long pulseDurationHigh = pulseIn(pin, HIGH);
+    //the pulse width is fixed to 38uS(typical);
+    unsigned long pulseDurationLow = pulseIn(pin, LOW);
+    if (pulseDurationLow > 0) {
+        return 38 + pulseDurationLow;
+    }
+    return 0;
+}
+
+void BL0937::calcEnergy(){
+    readActivePower();
+    unsigned long currentTime = millis();
+    unsigned long elapsedTime = currentTime - _lastMeasurementTime;
+    _lastMeasurementTime = currentTime;
+    _accumulatedEnergy += (float)(_activePower * elapsedTime ) / 3600000.0;
+}
+
+void BL0937::handleSysTimer(uint32_t ticks){
+    // (void) ticks;
+    if ((ticks % 10) == 0)
+        calcEnergy();
+}

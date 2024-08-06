fafdf4ff9c1060f95769eda09add66b1dbb48e57
amatilda <kpk@z-wave.me>
2024-08-07 00:40:48 +0300

    unlock speed

--- hardware/arduino/zunoG2/libraries/SpiFlash/SpiFlash.cpp
+++ hardware/arduino/zunoG2/libraries/SpiFlash/SpiFlash.cpp
@@ -275,8 +275,7 @@ bool SpiFlashClass::_init_end(const SpiFlashClassDevice_t *device, SpiFlashClass
 	if (manufacturer_id != device->manufacturer_id || memory_type != device->memory_type || capacity != device->capacity)
 		return (this->_last_status(STATUS_CONSTRUCTOR(STATUS_SEV_ERROR, STATUS_FACILITY_SPI_FLASH, STATUS_ID_NOT_MATCH), false));
 	this->_device = device;
-	// refFreq = CMU_ClockFreqGet(cmuClock_HFPER) - 0x1 / 0x2;
-	refFreq = 8 * SPI_FLASH_CLASS_CLOCK_MULTIPLIER;
+	refFreq = CMU_ClockFreqGet(cmuClock_USART0) - 0x1 / 0x2;
 	clock = device->max_clock_speed_mhz * SPI_FLASH_CLASS_CLOCK_MULTIPLIER;
 	if (clock > refFreq)
 		clock = refFreq;

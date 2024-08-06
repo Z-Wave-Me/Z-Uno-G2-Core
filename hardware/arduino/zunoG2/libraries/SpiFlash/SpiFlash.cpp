902a0548ac28e91f88f61e3bdda310d3af1b3443
amatilda <kpk@z-wave.me>
2024-08-06 17:20:12 +0300

    ss(9) to ss(SS)

--- hardware/arduino/zunoG2/libraries/SpiFlash/SpiFlash.cpp
+++ hardware/arduino/zunoG2/libraries/SpiFlash/SpiFlash.cpp
@@ -25,7 +25,7 @@ static const SpiFlashClassDevice_t					_device_all[] =
 };
 
 /* Public Constructors */
-SpiFlashClass::SpiFlashClass(void): _device(0x0), _spi(&SPI), _clock(4000000), _sck(SCK), _miso(MISO), _mosi(MOSI), _ss(9), _addr_byte(0x2) {
+SpiFlashClass::SpiFlashClass(void): _device(0x0), _spi(&SPI), _clock(4000000), _sck(SCK), _miso(MISO), _mosi(MOSI), _ss(SS), _addr_byte(0x2) {
 }
 
 SpiFlashClass::SpiFlashClass(SPIClass *spi, uint8_t sck, uint8_t miso, uint8_t mosi, uint8_t ss): _device(0x0), _spi(spi), _clock(4000000), _sck(sck), _miso(miso), _mosi(mosi), _ss(ss), _addr_byte(0x2) {
